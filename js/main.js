87b7501f48f5a652c0eb19bd49484a8d9234da3d
huykon225 <huykon225@gmail.com>
2018-07-16 18:01:14 +0700

    - update

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@
         var divs = $('.choose__options-fit').find('.fit_item_ins'),
             len = divs.length,
             randomDiv,
-            speed = 500;
+            speed = 1500;
 
         var interval = setInterval(
             function () {
@@ -113,73 +113,129 @@
                     $('li', this).eq(data_visible - 1).nextAll().hide().addClass('ins-toggleable');
                 }
             }
-
         });
 
 
         $('.ins_each--item').on('click', '.ins_benefit--viewmore', function () {
             var pane_id = $(this).closest('.ins_each--item').data('pane_id');
+
+
+            if( $(this).hasClass('less') ){
+                $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + '] button.ins_benefit--viewmore').text('Xem thêm').removeClass('less');
+                $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + ']').removeClass('has__view-more');
+            }else{
+                $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + '] button.ins_benefit--viewmore').text('Xem ít').addClass('less');
+                $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + ']').addClass('has__view-more');
+            }
+
             $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + ']').css('height','auto');
-            $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + '] li.ins-toggleable').slideDown();
-            $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + '] button.ins_benefit--viewmore').slideUp();
-            $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + ']').removeClass('has__view-more');
-            Adr_Ins_EqualHeight('.ins__equal-item');
+            $(this).closest('.tab-pane').find('.ins_each--item[data-pane_id=' + pane_id + '] li.ins-toggleable').slideToggle();
+            Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
         });
     }
 
-    $.fn.equalHeights = function(){
-        var max_height = 0;
-        $(this).each(function(){
-            max_height = Math.max($(this).height(), max_height);
+    $.fn.Ins_equalHeights = function(o){
+        /*var selector = this;
+        var heights = [];
+
+        selector.each(function(){
+            var height = $(this).height();
+            heights.push(height);
         });
-        $(this).each(function(){
-            $(this).height(max_height);
-        });
+
+        var maxHeight = Math.max.apply(null, heights);
+        console.log(heights,maxHeight);
+
+        selector.each(function(){
+            $(this).height(maxHeight);
+        });*/
+
+        var s = $.extend({
+            mobile: 767 // Mobile breakpoint
+        },o);
+        var w = Math.max(document.documentElement.clientWidth,window.innerWidth || 0); // Get the viewport width
+        this.css('height','auto'); // Make sure an explicit height isn't declared
+        if(w > s.mobile){ // If we're NOT on a mobile
+            var h = 0; // Initialize height variable
+            this.each(function(){ // For each matched element...
+                h = Math.max(h,$(this).outerHeight()); // Set h equal to its height, if it's taller than h
+            }).css('height',h); // Set all matched elements' height equal to h
+        }
+        return this;
     };
 
-    function Adr_Ins_EqualHeight(item) {
+    function Adr_Ins_EqualHeight(item, data_pane) {
         $('.ins__package-content').each(function () {
             var dt_max_items = $(this).data('max__items');
             if (typeof dt_max_items !== 'undefined' && dt_max_items > 0) {
                 for (var i = 0; i < dt_max_items; i++) {
-                    $(item+'[data-pane_id=' + i + ']').equalHeights();
+                    $(item+'['+data_pane+'=' + i + ']').Ins_equalHeights();
                 }
             }
         });
     }
 
-    /*$.fn.equalHeights1 = function(){
-        var max_height = 0;
-        $(this).each(function(){
-            max_height = Math.max($(this).height(), max_height);
+    function Adr_Ins_Accordion(){
+        $('.ins_item__accordion').on('shown.bs.collapse', function () {
+            Adr_Ins_EqualHeight('.ins_item__accordion', 'data-ins_accordion');
+            Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
         });
-        $(this).each(function(){
-            $(this).height(max_height);
+        $('.ins_item__accordion').on('hidden.bs.collapse', function () {
+            Adr_Ins_EqualHeight('.ins_item__accordion', 'data-ins_accordion');
+            Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
         });
-    };
+    }
 
-    function Adr_Ins_EqualHeight1() {
-        $('.ins__package-content').each(function () {
-            var dt_max_items = $(this).data('max__items');
-            if (typeof dt_max_items !== 'undefined' && dt_max_items > 0) {
-                for (var i = 0; i < dt_max_items; i++) {
-                    $('.ins__equal-item[data-pane_id=' + i + ']').equalHeights1();
-                }
+    function Adr_Ins_CheckedBenefit(){
+        if ( $('input.ins-styled-checkbox').is(":checked")){
+            $('input.ins-styled-checkbox:checked').closest('.ins__bene--child').addClass('active');
+        }else{
+            $('input.ins-styled-checkbox:checked').closest('.ins__bene--child').removeClass('active');
+        }
+    }
+
+    function Adr_Ins_CheckedBenefit_Change(){
+        $('.ins-styled-checkbox').on('change', function () {
+            if ( $(this).prop("checked")){
+                $(this).closest('.ins__bene--child').addClass('active');
+                Adr_Ins_EqualHeight('.ins__bor_bottom .ins_bene_line', 'data-ins_bene');
+                Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
+            }else{
+                $(this).closest('.ins__bene--child').removeClass('active');
+                Adr_Ins_EqualHeight('.ins__bor_bottom .ins_bene_line', 'data-ins_bene');
+                Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
             }
-            console.log($('#ins__accordion-detail_11').height());
         });
-    }*/
+    }
 
-    function Ard_Ins_Accordion(){
-        $('.ins_item__accordion').on('show.bs.collapse', function () {
-            Adr_Ins_EqualHeight('.pk__main_col--detail-information');
-        });
-        $('.ins_item__accordion').on('hide.bs.collapse', function () {
-            Adr_Ins_EqualHeight('.pk__main_col--detail-information');
-        });
+    function Adr_Ins_ChangeOld(){
+        $('.ins__change-age').on('click', function () {
+            var el = $(this),
+            parent = el.closest('.notify_bar_content');
+            parent.find('.ins__show_chosen').slideUp(300);
+            parent.find('form.comf_view__result').removeClass('hide');
+        })
+    }
+
+    function Adr_Ins_StickThis() {
+        if ($('.ins__nav-tabs--content').length > 0 && $('.ins--page-heading').length > 0) {
+            var $heding_height = $('.ins--page-heading').outerHeight();
+            $('.ins--page-heading').css('top', -$heding_height + 'px');
+
+            if ($(window).width() >= 768) {
+                var $tab_st = $('.ins__nav-tabs--content');
+                var $tab_st_pos = $tab_st.position();
+
+                $(window).on("scroll", function () {
+                    var fromTop = $(window).scrollTop();
+                    $('.ins--page-heading').toggleClass("ins_is-sticky", (fromTop > $tab_st_pos.top));
+                });
+            }
+        }
     }
 
     $(document).ready(function () {
+
         Adr_Ins_Swiper();
 
         Adr_Ins_openMenu();
@@ -190,7 +246,22 @@
         Adr_Ins_ScrollSelect();
         Adr_Ins_Showmore();
 
-        Adr_Ins_EqualHeight('.ins__equal-item');
-        Ard_Ins_Accordion();
+        Adr_Ins_EqualHeight('.ins__equal-item', 'data-pane_id');
+        Adr_Ins_EqualHeight('.ins__bor_bottom .ins_bene_line', 'data-ins_bene');
+        Adr_Ins_Accordion();
+
+        Adr_Ins_CheckedBenefit();
+        Adr_Ins_CheckedBenefit_Change();
+
+        Adr_Ins_ChangeOld();
+
+        Adr_Ins_StickThis();
+
     });
+
+    $(window).on('resize', function(){
+        Adr_Ins_StickThis();
+
+    });
+
 })(jQuery);
